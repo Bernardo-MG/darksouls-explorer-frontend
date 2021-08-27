3f470c201d1784d22ac00b729ec52fc62c7a2298
Bernardo <programming@bernardomg.com>
2021-08-27 22:19:53 +0200

    Added marker builder

--- src/app/graph-display/libs/display/graph.ts
+++ src/app/graph-display/libs/display/graph.ts
@@ -11,11 +11,10 @@ import { NodeBuilder } from './components/node-builder';
 import { NodeLabelBuilder } from './components/node-label-builder';
 import { LinksBuilder } from './components/link-builder';
 import { GraphViewBuilder } from './components/graph-view-builder';
-
-var config: DisplayConfig;
+import { MarkerBuilder } from './components/marker-builder';
 
 export function display(graph: DisplayGraph, selectNode = new EventEmitter<Number>(), currentZoom: number) {
-    config = new DisplayConfig(graph, (item: any) => selectNode.emit(item));
+    const config = new DisplayConfig(graph, (item: any) => selectNode.emit(item));
     const simulation = d3.forceSimulation(graph.nodes)
         .force("link", d3.forceLink(graph.links).id((d: any) => d.id))
         .force("charge", d3.forceManyBody())
@@ -25,6 +24,7 @@ export function display(graph: DisplayGraph, selectNode = new EventEmitter<Numbe
 
     const builders: ElementBuilder[] = [];
     builders.push(new GraphViewBuilder());
+    builders.push(new MarkerBuilder());
     builders.push(new LinksBuilder());
     builders.push(new NodeRootBuilder());
     builders.push(new NodeBuilder());
@@ -35,31 +35,13 @@ export function display(graph: DisplayGraph, selectNode = new EventEmitter<Numbe
         builder.bindToSimulation(rootView, simulation);
     }
 
-    setMarkers(rootView.select('#graph_view'), graph.types, config.color);
-    setZoom(rootView.select('#graph_view'), rootView.selectAll('.graph_link'), rootView.selectAll('#graph_nodes_root g'), currentZoom);
-}
-
-function setMarkers(mainView: Selection<SVGSVGElement, unknown, HTMLElement, any>,
-    types: String[], color: ScaleOrdinal<String, string, never>) {
-    // Per-type markers, as they don't inherit styles.
-    mainView.append("defs").selectAll("marker")
-        .data(types)
-        .join("marker")
-        .attr("id", d => `arrow-${d}`)
-        .attr("viewBox", "0 -5 10 10")
-        .attr("refX", 15)
-        .attr("refY", -0.5)
-        .attr("markerWidth", 6)
-        .attr("markerHeight", 6)
-        .attr("orient", "auto")
-        .append("path")
-        .attr("fill", color)
-        .attr("d", "M0,-5L10,0L0,5");
+    setZoom(rootView.select('#graph_view'), rootView.selectAll('.graph_link'), rootView.selectAll('#graph_nodes_root g'), config, currentZoom);
 }
 
 function setZoom(mainView: Selection<SVGSVGElement, unknown, HTMLElement, any>,
     link: Selection<any, any, any, any>,
     nodeRoot: Selection<any, any, any, any>,
+    config: DisplayConfig,
     currentZoom: number) {
     // Adds zoom
     const zoom: ZoomBehavior<Element, any> = d3.zoom()
