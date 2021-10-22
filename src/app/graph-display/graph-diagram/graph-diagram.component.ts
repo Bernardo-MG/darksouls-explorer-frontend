ba76fd4102cfa9e889bd5b8c0bb0c36ef7425743
Bernardo <programming@bernardomg.com>
2021-10-23 00:38:57 +0200

    Loads data from the backend

--- src/app/graph-display/graph-diagram/graph-diagram.component.ts
+++ src/app/graph-display/graph-diagram/graph-diagram.component.ts
@@ -22,49 +22,19 @@ export class GraphDiagramComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(): void {
-    // this.reload();
+    this.reload();
   }
 
   private reload(): void {
-    const nodes: NodeDefinition[] = this.graph.nodes.map(node => {return {data: { id: node.id.toString() } }})
-    const links = this.graph.links.map(link => {return {data: {  source: link.source, target: link.target }}})
+    const nodes: NodeDefinition[] = this.graph.nodes.map(node => { return { data: { id: node.id.toString() } } })
+    const links = this.graph.links.map(link => { return { data: { source: link.source.toString(), target: link.target.toString() } } })
 
     var cy = cytoscape({
       container: document.getElementById('graph_container'),
 
       elements: {
-        nodes: [
-          { data: { id: 'A' } },
-          { data: { id: 'B' } },
-          { data: { id: 'C' } },
-          { data: { id: 'D' } },
-          { data: { id: 'E' } },
-          { data: { id: 'F' } },
-          { data: { id: 'G' } },
-          { data: { id: 'H' } },
-          { data: { id: 'J' } },
-          { data: { id: 'K' } },
-          { data: { id: 'L' } },
-          { data: { id: 'M' } }
-        ],
-        edges: [
-          { data: {  source: 'A', target: 'B' } },
-          { data: {  source: 'A', target: 'C' } },
-          { data: { source: 'B', target: 'D' } },
-          { data: {  source: 'C', target: 'D' } },
-          { data: {  source: 'C', target: 'E' } },
-          { data: {  source: 'C', target: 'F' } },
-          { data: { source: 'D', target: 'G' } },
-          { data: { source: 'D', target: 'H' } },
-          { data: {  source: 'E', target: 'H' } },
-          { data: {  source: 'E', target: 'J' } },
-          { data: { source: 'F', target: 'J' } },
-          { data: {  source: 'F', target: 'K' } },
-          { data: {  source: 'G', target: 'L' } },
-          { data: {  source: 'H', target: 'L' } },
-          { data: {  source: 'H', target: 'M' } },
-          { data: {  source: 'J', target: 'M' } }
-        ]
+        nodes: nodes,
+        edges: links
       },
       style: [ // the stylesheet for the graph
         {
