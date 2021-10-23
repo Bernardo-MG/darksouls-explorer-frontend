f3a39e96521f8ab8480289d557aa6c386ede97c0
Bernardo <programming@bernardomg.com>
2021-10-23 16:54:53 +0200

    Added client provider to tests

--- src/app/graph-display/services/graph.service.spec.ts
+++ src/app/graph-display/services/graph.service.spec.ts
@@ -1,15 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 import { GraphDisplayModule } from '../graph-display.module';
 
+import { HttpClient } from '@angular/common/http';
+
 import { GraphService } from './graph.service';
 
 describe('GraphService', () => {
   let service: GraphService;
+  let httpClientSpy: { get: jasmine.Spy };
 
   beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+
     TestBed.configureTestingModule({
       imports: [
         GraphDisplayModule
+      ],
+      providers: [
+        { provide: HttpClient, useValue: httpClientSpy }
       ]
     });
     service = TestBed.inject(GraphService);
