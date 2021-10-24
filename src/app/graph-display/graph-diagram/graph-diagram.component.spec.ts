0ce8c89cfd5954015aca8b127c685d7b4980a89a
Bernardo <programming@bernardomg.com>
2021-10-24 23:03:17 +0200

    Added Cytoscape initialization to test

--- src/app/graph-display/graph-diagram/graph-diagram.component.spec.ts
+++ src/app/graph-display/graph-diagram/graph-diagram.component.spec.ts
@@ -2,11 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GraphDiagramComponent } from './graph-diagram.component';
 
+import * as cytoscape from 'cytoscape';
+// @ts-ignore
+import * as klay from 'cytoscape-klay';
+
 describe('GraphDiagramComponent', () => {
   let component: GraphDiagramComponent;
   let fixture: ComponentFixture<GraphDiagramComponent>;
 
   beforeEach(async () => {
+    cytoscape.use(klay);
     await TestBed.configureTestingModule({
       declarations: [ GraphDiagramComponent ]
     })
