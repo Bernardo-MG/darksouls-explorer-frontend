a31a945f92cead201a3358ceb59821a21ee5f13b
Bernardo <programming@bernardomg.com>
2021-11-27 16:59:00 +0100

    Max number of buttons visible for pagination

--- src/app/pagination/pagination-navigation/pagination-navigation.component.ts
+++ src/app/pagination/pagination-navigation/pagination-navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { SelectionPage } from '@app/pagination/models/SelectionPage';
+import { number } from 'echarts';
 
 @Component({
   selector: 'pagination-navigation',
@@ -26,11 +27,36 @@ export class PaginationNavigationComponent implements OnChanges {
 
   pages: SelectionPage[] = [];
 
+  skipBefore: boolean = false;
+
+  skipAfter: boolean = false;
+
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges): void {
+    var start: number;
+    var end: number;
+
+    if (this.currentPage <= this.rangeSize) {
+      start = 0;
+    } else {
+      start = this.currentPage - this.rangeSize;
+    }
+
+    if (this.totalPages <= this.rangeSize) {
+      end = this.totalPages;
+    } else {
+      end = this.currentPage + this.rangeSize;
+      if (end > this.totalPages) {
+        end = this.totalPages;
+      }
+    }
+
+    this.skipBefore = start > 0;
+    this.skipAfter = end < this.totalPages;
+
     this.pages = [];
-    for (var i = 0; i < this.totalPages; i++) {
+    for (var i = start; i < end; i++) {
       this.pages.push({ number: i, selected: i == this.currentPage });
     }
   }
