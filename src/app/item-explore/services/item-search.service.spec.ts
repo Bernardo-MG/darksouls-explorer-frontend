7a5a8468ddfea158b2af07e4ba12c4bdd1a1c62a
Bernardo <programming@bernardomg.com>
2021-12-09 16:21:34 +0100

    Fixed test imports

--- src/app/item-explore/services/item-search.service.spec.ts
+++ src/app/item-explore/services/item-search.service.spec.ts
@@ -1,12 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ItemSearchService } from './item-search.service';
 
 describe('ItemSearchService', () => {
   let service: ItemSearchService;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        ItemSearchService
+      ]
+    });
+    httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(ItemSearchService);
   });
 
