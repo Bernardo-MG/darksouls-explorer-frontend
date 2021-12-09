42b7be4be9295dd1d3349c89408a0a8d43782b88
Bernardo <programming@bernardomg.com>
2021-12-09 15:45:27 +0100

    Added provider to test

--- src/app/item-explore/item-view/item-view.component.spec.ts
+++ src/app/item-explore/item-view/item-view.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ItemSearchService } from '../services/item-search.service';
 import { ItemService } from '../services/item.service';
 
 import { ItemViewComponent } from './item-view.component';
@@ -19,7 +20,8 @@ describe('ItemViewComponent', () => {
         ItemViewComponent
       ],
       providers: [
-        ItemService
+        ItemService,
+        ItemSearchService
       ]
     })
     .compileComponents();
