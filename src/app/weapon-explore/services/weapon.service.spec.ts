9fc06bd7aadb414c1a07d5122af67170c97450ee
Bernardo <programming@bernardomg.com>
2022-03-06 13:25:49 +0100

    Fixed test configuration

--- src/app/weapon-explore/services/weapon.service.spec.ts
+++ src/app/weapon-explore/services/weapon.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { WeaponService } from './weapon.service';
 
 describe('WeaponService', () => {
   let service: WeaponService;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        WeaponService
+      ]
+    });
+    httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(WeaponService);
   });
 
