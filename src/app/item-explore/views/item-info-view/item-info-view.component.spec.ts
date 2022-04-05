26d7bd4177d41ae40b75bac6dcb3cbe0b5c8556e
Bernardo <programming@bernardomg.com>
2022-04-05 20:01:28 +0200

    Fixed test dependencies

--- src/app/item-explore/views/item-info-view/item-info-view.component.spec.ts
+++ src/app/item-explore/views/item-info-view/item-info-view.component.spec.ts
@@ -1,16 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { ItemService } from '@app/item-explore/services/item.service';
 import { ItemInfoViewComponent } from './item-info-view.component';
 
 describe('ItemInfoViewComponent', () => {
   let component: ItemInfoViewComponent;
   let fixture: ComponentFixture<ItemInfoViewComponent>;
+  let httpClient: HttpClient;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ItemInfoViewComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      declarations: [
+        ItemInfoViewComponent
+      ],
+      providers: [
+        ItemService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+    httpClient = TestBed.inject(HttpClient);
   });
 
   beforeEach(() => {
--- src/app/item-explore/views/item-list-view/item-list-view.component.spec.ts
+++ src/app/item-explore/views/item-list-view/item-list-view.component.spec.ts
@@ -1,16 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { ItemSearchService } from '@app/item-explore/services/item-search.service';
+import { ItemService } from '@app/item-explore/services/item.service';
 import { ItemListViewComponent } from './item-list-view.component';
 
 describe('ItemListViewComponent', () => {
   let component: ItemListViewComponent;
   let fixture: ComponentFixture<ItemListViewComponent>;
+  let httpClient: HttpClient;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ItemListViewComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      declarations: [
+        ItemListViewComponent
+      ],
+      providers: [
+        ItemService,
+        ItemSearchService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+      httpClient = TestBed.inject(HttpClient);
   });
 
   beforeEach(() => {
