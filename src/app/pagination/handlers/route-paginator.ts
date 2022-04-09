562678e9806db30f5eeff3c30028bf5821151419
Bernardo <programming@bernardomg.com>
2022-04-09 21:17:13 +0200

    Reduced rout paginator

--- src/app/pagination/handlers/route-paginator.ts
+++ src/app/pagination/handlers/route-paginator.ts
@@ -1,16 +1,7 @@
 import { Router } from "@angular/router";
-import { PageInfo } from "../models/page-info";
 import { DefaultPaginator } from "./default-paginator";
 
 export class RoutePaginator extends DefaultPaginator {
-
-    public currentPage: number = 0;
-
-    public totalPages: number = 0;
-
-    public previousEnabled: boolean = false;
-
-    public nextEnabled: boolean = false;
 
     private path: string;
 
@@ -18,27 +9,8 @@ export class RoutePaginator extends DefaultPaginator {
         private router: Router
     ) {
         super();
+
         this.path = this.router.url.split('?')[0];
-    }
-
-    public setPagination(page: PageInfo): void {
-        this.currentPage = page.pageNumber;
-        this.totalPages = page.totalPages;
-
-        this.previousEnabled = !page.first;
-        this.nextEnabled = !page.last;
-    }
-
-    public toFirstPage(): void {
-        this.toPage(0);
-    }
-
-    public toPreviousPage(): void {
-        this.toPage(this.currentPage - 1);
-    }
-
-    public toNextPage(): void {
-        this.toPage(this.currentPage + 1);
     }
 
     public toPage(page: number): void {
