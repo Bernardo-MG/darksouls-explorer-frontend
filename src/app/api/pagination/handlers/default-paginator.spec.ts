c5918e2a8e186a4dab92ed8e9cb80ba6d8abf0f7
Bernardo <programming@bernardomg.com>
2022-04-09 22:32:00 +0200

    Fixed test initialization

--- src/app/api/pagination/handlers/default-paginator.spec.ts
+++ src/app/api/pagination/handlers/default-paginator.spec.ts
@@ -1,9 +1,7 @@
-import { PaginatedResponse } from '@app/api/request/models/paginated-response';
-import { of } from 'rxjs';
 import { DefaultPaginator } from './default-paginator';
 
 describe('Paginator', () => {
   it('should create an instance', () => {
-    expect(new DefaultPaginator((number) => of(new PaginatedResponse(undefined)))).toBeTruthy();
+    expect(new DefaultPaginator()).toBeTruthy();
   });
 });
