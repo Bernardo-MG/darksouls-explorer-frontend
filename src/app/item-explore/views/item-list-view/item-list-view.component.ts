b8afb4c36b4739bedad3b7dc668ca9d6486167ec
Bernardo <programming@bernardomg.com>
2022-04-10 19:08:26 +0200

    Removed unused imports

--- src/app/item-explore/views/item-list-view/item-list-view.component.ts
+++ src/app/item-explore/views/item-list-view/item-list-view.component.ts
@@ -3,11 +3,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Datasource } from '@app/api/datasource/handlers/datasource';
 import { DatasourceBuilder } from '@app/api/datasource/handlers/datasource-builder';
 import { Paginator } from '@app/api/pagination/handlers/paginator';
-import { Response } from '@app/api/request/models/response';
 import { ItemService } from '@app/item-explore/services/item.service';
 import { Item } from '@app/models/item';
 import { ItemSearch } from '@app/models/itemSearch';
-import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-item-list-view',
